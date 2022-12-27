cb38f072879a191f06cccabccd371265d4820f1c
kodammanikiran <37047636+kodammanikiran@users.noreply.github.com>
2022-12-27 16:40:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,15 +31,20 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamli.error()
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("use warehouse compute_wh")
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("FruitLoad list contains")
-streamlit.dataframe(my_data_rows)
 
+
+streamlit.header("FruitLoad list contains")
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("use warehouse compute_wh")
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+streamlit.dataframe(my_data_rows)
+if streamlit.button("Get Fruit List"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to?','')
 streamlit.write('Thanks for adding ', add_my_fruit)
 my_cur.execute("use warehouse compute_wh")
